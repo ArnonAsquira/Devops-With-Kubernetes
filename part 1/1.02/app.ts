35ee8ed01268e2075edc81649b9477e13737ec49
ArnonAsquira <arnonafking2010@gmail.com>
2022-03-23 17:30:32 +0200

    advancing to part 1.3

--- part 1/1.02/app.ts
+++ part 1/1.02/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import path from "path";
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./front/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
